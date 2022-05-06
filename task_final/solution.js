970735a3a29d13d022c3973452cf5dfc4c38fc62
Irina Mankova <irina.mankova14@gmail.com>
2022-05-06 10:10:03 +0300

    Task final!

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,15 +34,15 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name: '', phone: ''}, order: {address: '', sum: ''},  goods: []}
+    let data = {client: {}, order: {address: '', sum: ''},  goods: []}
  
-    data.goods = [{title:goods.title}, {count:goods.count}]
-    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв" + address.flat;
+    data.client = name + ' ' + phone;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
     data.order.sum = sum;
 
-    let countOfGoods = data.goods.length;
+    let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
